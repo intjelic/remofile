556fc8645d0a8444b826a7ad17dae73c6a830767
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2018-09-30 01:20:34 +0700

    Implemented generate_keys() function

--- remofile/__init__.py
+++ remofile/__init__.py
@@ -7,4 +7,5 @@ from remofile.algorithm import synchronize_upload, synchronize_download
 from remofile.exceptions import *
 
 from remofile.token import generate_token
+from remofile.keys import generate_keys
 from remofile.utils import is_file_name_valid
---
+++ remofile/keys.py
@@ -0,0 +1,16 @@
+# Remofile - Quick and easy-to-use alternative to FTP
+#
+# This file is distributed under the MIT License. See the LICENSE file
+# in the root of this project for more information.
+#
+# Written by Jonathan De Wachter <dewachter.jonathan@gmail.com>, September 2018
+
+import zmq
+
+def generate_keys():
+    """ Brief description.
+
+    Long description.
+    """
+
+    return zmq.curve_keypair()
